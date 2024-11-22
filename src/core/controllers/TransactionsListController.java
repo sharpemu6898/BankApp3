3a0a48d683f7c8dd03eb11f41ffdb11ff72e1d5b
sharpemu6898 <133412975+sharpemu6898@users.noreply.github.com>
2024-11-22 17:01:02 -0500

    cambio transacciones
    cambios menores

--- src/core/controllers/TransactionsListController.java
+++ src/core/controllers/TransactionsListController.java
@@ -6,7 +6,10 @@ package core.controllers;
 
 import core.controllers.utils.Response;
 import core.controllers.utils.Status;
+import core.models.Transaction;
 import core.models.storage.TransactionsStorage;
+import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  *
@@ -20,8 +23,9 @@ public class TransactionsListController {
             if(storage.getTransactions().isEmpty()){
                 return new Response("There are no registrated transactions to show",Status.NOT_FOUND);
             }else{
-                storage.getTransactions().sort((obj1, obj2) -> (obj1.getAmount() - obj2.getAmount()));
-                return new Response("Showing transactions list",Status.OK, storage.getTransactions());
+                ArrayList<Transaction> transactionsCopy = (ArrayList<Transaction>) storage.getTransactions().clone();
+                Collections.reverse(transactionsCopy);
+                return new Response("Showing transactions list",Status.OK, transactionsCopy);
             }
         }catch(Exception ex){
             return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
