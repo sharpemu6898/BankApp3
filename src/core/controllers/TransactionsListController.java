4e772fc7d186589eb54838d5621fe5efad65132d
sharpemu6898 <133412975+sharpemu6898@users.noreply.github.com>
2024-11-22 16:35:40 -0500

    cambios lista de transacciones
    algunos cambios

--- src/core/controllers/TransactionsListController.java
+++ src/core/controllers/TransactionsListController.java
@@ -4,11 +4,29 @@
  */
 package core.controllers;
 
+import core.controllers.utils.Response;
+import core.controllers.utils.Status;
+import core.models.storage.TransactionsStorage;
+
 /**
  *
  * @author AAAAA
  */
 public class TransactionsListController {
     
-    falta
+    public static Response listTransactions(){
+        try{
+            TransactionsStorage storage=TransactionsStorage.getInstance();
+            if(storage.getTransactions().isEmpty()){
+                return new Response("There are no registrated transactions to show",Status.NOT_FOUND);
+            }else{
+                storage.getTransactions().sort((obj1, obj2) -> (obj1.getAmount() - obj2.getAmount()));
+                return new Response("Showing transactions list",Status.OK, storage.getTransactions());
+            }
+        }catch(Exception ex){
+            return new Response("Unexpected error", Status.INTERNAL_SERVER_ERROR);
+        }
+    
+    
+    }
 }
